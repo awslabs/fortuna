fa43bf969b20608c8e04848b685cd97bdae5b899
chleibig <leibig.christian@gmail.com>
2022-12-31 15:35:17 +0100

    Private prefetch

--- tests/fortuna/test_data.py
+++ tests/fortuna/test_data.py
@@ -3,8 +3,7 @@ import unittest
 import numpy as np
 
 from fortuna.data.loader import DataLoader, InputsLoader, TargetsLoader
-from tests.make_data import (make_generator_fun_random_data,
-                             make_generator_random_data)
+from tests.make_data import make_array_random_data, make_generator_fun_random_data, make_generator_random_data
 
 
 class Test(unittest.TestCase):
@@ -35,6 +34,18 @@ class Test(unittest.TestCase):
         for a in inputs:
             assert type(a) == np.ndarray
             assert a.shape == (3, 5)
+
+    def test_array_data(self):
+        data_org = make_array_random_data(
+            n_data=10,
+            shape_inputs=(2,),
+            output_dim=1,
+            output_type="continuous",
+        )
+        for prefetch in [False, True]:
+            data_new = DataLoader.from_array_data(data_org, batch_size=2, prefetch=prefetch).to_array_data()
+            for original, new in zip(data_org, data_new):
+                assert (original == new).all()
 
 
 class TestGetTargets(unittest.TestCase):
