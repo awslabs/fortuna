028a63dcdf83d3ca6ff8f448385e1c762b90f305
Gianluca Detommaso <detommaso.gianluca@gmail.com>
2023-04-01 19:43:43 +0200

    add functionalities to data loader

--- tests/fortuna/test_data.py
+++ tests/fortuna/test_data.py
@@ -80,3 +80,29 @@ class TestGetTargets(unittest.TestCase):
         for a in targets:
             assert type(a) == np.ndarray
             assert a.shape == (3, 4)
+
+
+class TestDataLoaders(unittest.TestCase):
+    def test_data_loader_from_inputs_loader(self):
+        inputs1 = np.arange(10)
+        inputs2 = 1 + np.arange(7)
+        inputs_loaders = [
+            InputsLoader.from_array_inputs(inputs1, batch_size=3),
+            InputsLoader.from_array_inputs(inputs2, batch_size=4)
+        ]
+        targets = [0, 1]
+        data_loader = DataLoader.from_inputs_loader(inputs_loaders, targets)
+        for i, (x, y) in enumerate(data_loader):
+            assert x.shape == (7,) if i == 0 else (6,) if i == 1 else (3,) if i == 2 else (1,)
+            assert all(y[:3] == 0)
+            assert all(y[3:] == 1)
+            assert len(x) == len(y)
+
+    def test_inputs_loader_to_filtered_inputs_loader(self):
+        inputs = np.arange(10)
+        inputs_loader = InputsLoader.from_array_inputs(inputs, batch_size=3)
+        filtered_inputs_loader = inputs_loader.to_filtered_inputs_loader(lambda x: x[x < 7])
+
+        for i, x in enumerate(filtered_inputs_loader):
+            assert x.shape == (3,) if i < 2 else (1,)
+            assert all(x < 7)
