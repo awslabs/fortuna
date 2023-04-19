04a61c670c1894586a3a37c7c62d22d7c42c9dba
Oleg Smirnov <oleg.smirnov@gmail.com>
2023-04-19 21:17:20 +0200

    Fix random seeds (#30)
    * minor type hints fix
    
    * fix random seed in data loaders
    
    * fix random seed in tests

--- tests/make_data.py
+++ tests/make_data.py
@@ -1,42 +1,61 @@
-from typing import Callable, Generator, Tuple
+from typing import Callable, Generator, Tuple, Optional
 
 import numpy as np
 
 
+def _check_output_type(output_type):
+    if output_type not in ["discrete", "continuous"]:
+        raise Exception(
+            f"`output_type={output_type}` not recognized. " \
+            "Please choose among the following list: ['discrete', 'continuous']"
+        )
+
+
 def make_array_random_inputs(
-    n_inputs: int, shape_inputs: Tuple[int, ...]
+    n_inputs: int,
+    shape_inputs: Tuple[int, ...],
+    seed: Optional[int] = 0
 ) -> np.ndarray:
-    return np.random.normal(size=(n_inputs,) + shape_inputs)
+    rng = np.random.default_rng(seed)
+    return rng.normal(size=(n_inputs,) + shape_inputs)
 
 
 def make_generator_fun_random_inputs(
-    batch_size: int, n_batches: int, shape_inputs: Tuple[int, ...]
+    batch_size: int,
+    n_batches: int,
+    shape_inputs: Tuple[int, ...],
+    seed: Optional[int] = 0
 ) -> Callable[[], Generator[np.ndarray, None, None]]:
+    rng = np.random.default_rng(seed)
     def inner():
         for i in range(n_batches):
-            yield np.random.normal(size=(batch_size,) + shape_inputs)
-
+            yield rng.normal(size=(batch_size,) + shape_inputs)
     return inner
 
 
 def make_generator_random_inputs(
-    batch_size: int, n_batches: int, shape_inputs: Tuple[int, ...]
+    batch_size: int,
+    n_batches: int,
+    shape_inputs: Tuple[int, ...],
+    seed: Optional[int] = 0
 ) -> Generator[np.ndarray, None, None]:
+    rng = np.random.default_rng(seed)
     for i in range(n_batches):
-        yield np.random.normal(size=(batch_size,) + shape_inputs)
+        yield rng.normal(size=(batch_size,) + shape_inputs)
 
 
-def make_array_random_targets(n_inputs: int, output_dim: int, output_type: str):
-    if output_type not in ["discrete", "continuous"]:
-        raise Exception(
-            "`output_type={}` not recognized. Please choose among the following list: {}.".format(
-                "discrete", "continuous"
-            )
-        )
+def make_array_random_targets(
+    n_inputs: int,
+    output_dim: int,
+    output_type: str,
+    seed: Optional[int] = 0
+) -> np.ndarray:
+    _check_output_type(output_type)
+    rng = np.random.default_rng(seed)
     return (
-        np.random.normal(size=(n_inputs, output_dim))
+        rng.normal(size=(n_inputs, output_dim))
         if output_type == "continuous"
-        else np.random.choice(output_dim, size=n_inputs)
+        else rng.choice(output_dim, size=n_inputs)
     )
 
 
@@ -45,22 +64,17 @@ def make_generator_fun_random_targets(
     n_batches: int,
     output_dim: int,
     output_type: str,
+    seed: Optional[int] = 0,
 ) -> Callable[[], Generator[Tuple[np.ndarray, np.ndarray], None, None]]:
-    if output_type not in ["discrete", "continuous"]:
-        raise Exception(
-            "`output_type={}` not recognized. Please choose among the following list: {}.".format(
-                "discrete", "continuous"
-            )
-        )
-
+    _check_output_type(output_type)
+    rng = np.random.default_rng(seed)
     def inner():
         for i in range(n_batches):
-            yield np.random.normal(
+            yield rng.normal(
                 size=(batch_size, output_dim)
-            ) if output_type == "continuous" else np.random.choice(
+            ) if output_type == "continuous" else rng.choice(
                 output_dim, size=batch_size
             )
-
     return inner
 
 
@@ -69,35 +83,32 @@ def make_generator_random_targets(
     n_batches: int,
     output_dim: int,
     output_type: str,
+    seed: Optional[int] = 0,
 ) -> Generator[Tuple[np.ndarray, np.ndarray], None, None]:
-    if output_type not in ["discrete", "continuous"]:
-        raise Exception(
-            "`output_type={}` not recognized. Please choose among the following list: {}.".format(
-                "discrete", "continuous"
-            )
-        )
+    _check_output_type(output_type)
+    rng = np.random.default_rng(seed)
     for i in range(n_batches):
-        yield np.random.normal(
+        yield rng.normal(
             size=(batch_size, output_dim)
-        ) if output_type == "continuous" else np.random.choice(
+        ) if output_type == "continuous" else rng.choice(
             output_dim, size=batch_size
         )
 
 
 def make_array_random_data(
-    n_data: int, shape_inputs: Tuple[int, ...], output_dim: int, output_type: str
+    n_data: int,
+    shape_inputs: Tuple[int, ...],
+    output_dim: int,
+    output_type: str,
+    seed: Optional[int] = 0,
 ) -> Tuple[np.ndarray, np.ndarray]:
-    if output_type not in ["discrete", "continuous"]:
-        raise Exception(
-            "`output_type={}` not recognized. Please choose among the following list: {}.".format(
-                "discrete", "continuous"
-            )
-        )
+    _check_output_type(output_type)
+    rng = np.random.default_rng(seed)
     return (
-        np.random.normal(size=(n_data,) + shape_inputs),
-        np.random.normal(size=(n_data, output_dim))
+        rng.normal(size=(n_data,) + shape_inputs),
+        rng.normal(size=(n_data, output_dim))
         if output_type == "continuous"
-        else np.random.choice(output_dim, size=n_data),
+        else rng.choice(output_dim, size=n_data),
     )
 
 
@@ -107,19 +118,15 @@ def make_generator_fun_random_data(
     shape_inputs: Tuple[int],
     output_dim: int,
     output_type: str,
+    seed: Optional[int] = 0,
 ) -> Callable[[], Generator[Tuple[np.ndarray, np.ndarray], None, None]]:
-    if output_type not in ["discrete", "continuous"]:
-        raise Exception(
-            "`output_type={}` not recognized. Please choose among the following list: {}.".format(
-                "discrete", "continuous"
-            )
-        )
-
+    _check_output_type(output_type)
+    rng = np.random.default_rng(seed)
     def inner():
         for i in range(n_batches):
-            yield np.random.normal(size=(batch_size,) + shape_inputs), np.random.normal(
+            yield rng.normal(size=(batch_size,) + shape_inputs), rng.normal(
                 size=(batch_size, output_dim)
-            ) if output_type == "continuous" else np.random.choice(
+            ) if output_type == "continuous" else rng.choice(
                 output_dim, size=batch_size
             )
 
@@ -132,16 +139,13 @@ def make_generator_random_data(
     shape_inputs: Tuple[int],
     output_dim: int,
     output_type: str,
+    seed: Optional[int] = 0,
 ) -> Generator[Tuple[np.ndarray, np.ndarray], None, None]:
-    if output_type not in ["discrete", "continuous"]:
-        raise Exception(
-            "`output_type={}` not recognized. Please choose among the following list: {}.".format(
-                "discrete", "continuous"
-            )
-        )
+    _check_output_type(output_type)
+    rng = np.random.default_rng(seed)
     for i in range(n_batches):
-        yield np.random.normal(size=(batch_size,) + shape_inputs), np.random.normal(
+        yield rng.normal(size=(batch_size,) + shape_inputs), rng.normal(
             size=(batch_size, output_dim)
-        ) if output_type == "continuous" else np.random.choice(
+        ) if output_type == "continuous" else rng.choice(
             output_dim, size=batch_size
         )
