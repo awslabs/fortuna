49f416d723faa203dba3f9ff693ea44dff2277a4
Gianluca Detommaso <detommaso.gianluca@gmail.com>
2023-05-08 23:25:44 +0200

    swag can start from stats when restoring

--- tests/fortuna/prob_model/test_train.py
+++ tests/fortuna/prob_model/test_train.py
@@ -30,12 +30,12 @@ OUTPUT_DIM = 2
 
 TASKS = ["regression", "classification"]
 METHODS = {
-    "map": MAPPosteriorApproximator(),
-    "advi": ADVIPosteriorApproximator(),
-    "laplace": LaplacePosteriorApproximator(),
+    # "map": MAPPosteriorApproximator(),
+    # "advi": ADVIPosteriorApproximator(),
+    # "laplace": LaplacePosteriorApproximator(),
     "swag": SWAGPosteriorApproximator(rank=2),
-    "deep_ensemble": DeepEnsemblePosteriorApproximator(ensemble_size=2),
-    "sngp": SNGPPosteriorApproximator(output_dim=OUTPUT_DIM)
+    # "deep_ensemble": DeepEnsemblePosteriorApproximator(ensemble_size=2),
+    # "sngp": SNGPPosteriorApproximator(output_dim=OUTPUT_DIM)
 }
 TASKS_METHODS = [(task, method) for task in TASKS for method in METHODS if (task, method) != ("regression", "sngp")]
 TASKS_IDS = [t + "-" + m for t, m in TASKS_METHODS]
@@ -66,7 +66,7 @@ def test_dryrun(task, method):
 
     freeze_fun = lambda p, v: "trainable" if "l2" in p and "model" in p else "frozen"
 
-    fit_config = lambda restore_path, start_current, save_dir, freeze: FitConfig(
+    fit_config = lambda restore_path, start_current, save_dir, dump_state, save_n_steps, freeze: FitConfig(
             optimizer=FitOptimizer(
                 n_epochs=3,
                 freeze_fun=freeze
@@ -77,7 +77,9 @@ def test_dryrun(task, method):
             checkpointer=FitCheckpointer(
                 start_from_current_state=start_current,
                 restore_checkpoint_path=restore_path,
-                save_checkpoint_dir=save_dir
+                save_checkpoint_dir=save_dir,
+                dump_state=dump_state,
+                save_every_n_steps=save_n_steps
             )
         )
 
@@ -85,12 +87,12 @@ def test_dryrun(task, method):
         optimizer=CalibOptimizer(n_epochs=3)
     )
 
-    def train_and_sample(restore_path=None, start_current=False, save_dir=None, freeze=None, map_fit_config=None):
+    def train_and_sample(restore_path=None, start_current=False, save_dir=None, dump_state=False, save_n_steps=None, freeze=None, map_fit_config=None):
         prob_model.train(
             train_data_loader=train_data_loader,
             val_data_loader=val_data_loader,
             calib_data_loader=calib_data_loader,
-            fit_config=fit_config(restore_path, start_current, save_dir, freeze),
+            fit_config=fit_config(restore_path, start_current, save_dir, dump_state, save_n_steps, freeze),
             calib_config=calib_config,
             map_fit_config=map_fit_config
         )
@@ -111,13 +113,22 @@ def test_dryrun(task, method):
             posterior_approximator=METHODS[method]
         )
 
-    train_and_sample(map_fit_config=fit_config(restore_path=None, start_current=None, save_dir=None, freeze=None))
+    train_and_sample(
+        map_fit_config=fit_config(
+            restore_path=None,
+            start_current=None,
+            save_dir=None,
+            dump_state=False,
+            save_n_steps=None,
+            freeze=None
+        )
+    )
     train_and_sample(start_current=True)
     if method not in ["laplace", "swag"]:
         train_and_sample()
 
     with tempfile.TemporaryDirectory() as tmp_dir:
-        train_and_sample(map_fit_config=fit_config(restore_path=None, start_current=None, save_dir=None, freeze=None), save_dir=tmp_dir)
+        train_and_sample(map_fit_config=fit_config(restore_path=None, start_current=None, save_dir=None, dump_state=False, save_n_steps=None, freeze=None), save_dir=tmp_dir, save_n_steps=2, dump_state=True)
         train_and_sample(restore_path=tmp_dir)
         if method not in ["laplace", "swag"]:
             train_and_sample(freeze=freeze_fun)
